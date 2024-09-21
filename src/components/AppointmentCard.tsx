e71eb5b506bde112b5da1a6465749f3f6924ff96
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-21 13:50:43 +0100

    style adjustment

--- src/components/AppointmentCard.tsx
+++ src/components/AppointmentCard.tsx
@@ -1,62 +1,72 @@
-'use client'
-import React, { useState } from 'react';
-import Image from 'next/image';
-import AppointmentDetails from './AppointmentDetails'; // Import the new component
-import { AppointmentCardProps } from '@/types/AppointmentCardProps';
+"use client";
+import { AppointmentCardProps } from "@/types/AppointmentCardProps";
+import Image from "next/image";
+import { useState } from "react";
+import AppointmentDetails from "./AppointmentDetails"; // Import the new component
+import Button from "./ui/Button";
 
-const AppointmentCard = ({ image, name, date, raison }: AppointmentCardProps) => {
-  const [showDetails, setShowDetails] = useState(false);
+const AppointmentCard = ({
+	image,
+	name,
+	date,
+	raison,
+}: AppointmentCardProps) => {
+	const [showDetails, setShowDetails] = useState(false);
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
+	const toggleDetails = () => {
+		setShowDetails(!showDetails);
+	};
 
-  return (
-    <div className='border border-gray flex gap-2 flex-col rounded-xl p-6 relative'>
-      <div className='flex items-center justify-between flex-wrap gap-2'>
-        <div className='flex items-center gap-4'>
-          <Image src={`/images/${image}.png`} alt={name} width={50} height={50} />
-          <div className='flex flex-col'>
-            <span className='font-bold'>{name}</span>
-            <span className='font-light'>{date}</span>
-          </div>
-        </div>
-        <button
-          className='rounded-2xl py-1 px-2 bg-gray font-semibold text-sm'
-          onClick={toggleDetails}
-        >
-          View details
-        </button>
-      </div>
+	return (
+		<div className="border border-gray flex gap-2 flex-col rounded-xl p-6 relative">
+			<div className="flex items-center justify-between flex-wrap gap-2">
+				<div className="flex items-center gap-4">
+					<Image
+						src={`/images/${image}.png`}
+						alt={name}
+						width={50}
+						height={50}
+					/>
+					<div className="flex flex-col">
+						<span className="font-bold">{name}</span>
+						<span className="font-light">{date}</span>
+					</div>
+				</div>
+				<button
+					className="rounded-2xl flex justify-center items-center py-1.5 px-3 bg-gray font-semibold text-xs"
+					onClick={toggleDetails}
+				>
+					View details
+				</button>
+			</div>
 
-      <div className='lg:pr-6 pr-3 text-justify'>
-        <span className='font-semibold'>Raison: </span>
-        <span>{raison}</span>
-      </div>
+			<div className="lg:pr-6">
+				<h3 className="font-semibold">Raison: </h3>
+				<p className="text-sm sm:text-base">{raison}</p>
+			</div>
+			<div className="flex items-center flex-wrap justify-between gap-4">
+				<Button variant="outline" className="max-sm:w-full">
+					Decline appointment
+				</Button>
+				<Button variant="linear_color" className="max-sm:w-full">
+					Accept appointment
+				</Button>
+			</div>
 
-      <div className='flex items-center justify-between gap-2'>
-        <button className='border lg:p-2 p-1 rounded-md'>
-          Decline appointment
-        </button>
-        <button className='lg:p-2 p-1 text-white bg-gradient-to-r from-blue to-darkBlue rounded-md'>
-          Accept appointment
-        </button>
-      </div>
-
-      {/* Display AppointmentDetails component */}
-      {showDetails && (
-        <AppointmentDetails
-          image='user'
-          name={name}
-          adress='Preston Inglewood, Maine 98380'
-          date='Monday Jun 14, 2024 on 08 AM - 10 AM'
-          interest='Diabetes Control Appointment'
-          problem='Blood sugar management is especially important for people with diabetes as chronicaly high blood sugar levels can lead.'
-          onClose={toggleDetails} // Pass the close function as a prop
-        />
-      )}
-    </div>
-  );
+			{/* Display AppointmentDetails component */}
+			{showDetails && (
+				<AppointmentDetails
+					image="user"
+					name={name}
+					adress="Preston Inglewood, Maine 98380"
+					date="Monday Jun 14, 2024 on 08 AM - 10 AM"
+					interest="Diabetes Control Appointment"
+					problem="Blood sugar management is especially important for people with diabetes as chronicaly high blood sugar levels can lead."
+					onClose={toggleDetails} // Pass the close function as a prop
+				/>
+			)}
+		</div>
+	);
 };
 
 export default AppointmentCard;
