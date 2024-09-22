bad9150c44e04df72c392e40a8e9fb035897c068
GitOmonwalé <inesagbozo@gmail>
2024-09-22 21:14:16 +0100

    font ajustment of section CTA

--- src/components/Home/sections/CTA.tsx
+++ src/components/Home/sections/CTA.tsx
@@ -6,10 +6,10 @@ import { ChevronRight } from '@/assets/icons'
 const CTA = () => {
   return (
     <div className='flex items-center justify-between container my-10'>
-      <div className='bg-cta-bg h-96 container relative'>
-        <div className='max-w-lg mt-14 flex flex-col gap-4'>
+      <div className='bg-cta-bg h-80 container relative'>
+        <div className='max-w-xl mt-14 flex flex-col gap-4'>
           <h2 className='text-white font-bold text-5xl'>Don’t Let Your Health Take a Backseat!</h2>
-          <p>Schedule an appointment of of our experienced medical profesioonal today</p>
+          <p className='max-w-96'>Schedule an appointment of of our experienced medical profesioonal today</p>
           <Link
 					href="/sign-up"
 					className="bg-gradient-to-b from-[#307bc4] to-[#274760] text-white shadow-light-blue !rounded-3xl px-8 py-3 max-w-44 flex justify-center items-center gap-2.5"
