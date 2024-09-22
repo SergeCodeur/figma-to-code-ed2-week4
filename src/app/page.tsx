b3c856443bd381f6d8db33e465cd86e408698d84
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-22 14:27:27 +0100

    add navbar to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Departments from "@/components/Home/sections/Departments";
 import HeroSection from "@/components/Home/sections/HeroSection";
 import OurValues from "@/components/Home/sections/OurValues";
 import ReviewsSection from "@/components/Home/sections/Reviews";
+import NavBar from "@/components/Navigation/NavBar";
 
 export default function Home() {
 	return (
 		<main>
+			<NavBar />
 			<HeroSection />
 			<OurValues />
 			<AboutUs />
