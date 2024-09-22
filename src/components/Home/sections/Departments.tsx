e1456928fd4363a59807285be5e8c040c3d79071
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-22 13:42:01 +0100

    change file name from calmelCase to PascalCase

---
+++ src/components/Home/sections/Departments.tsx
@@ -0,0 +1,28 @@
+import DepartementCard from "@/components/cards/DepartementCard";
+import Title from "@/components/ui/Title";
+import { DepartmentsList } from "@/constants/Departement";
+
+const Departments = () => {
+	//
+	//
+
+	return (
+		<div className="mt-20 container w-full">
+			<div className="relative flex items-center justify-center flex-col">
+				<div className="w-full h-60 lg:h-80 bg-dep-bg bg-cover bg-no-repeat rounded-3xl absolute top-0 left-0 -z-10"></div>
+				<div className="bg-transparent flex items-start justify-between flex-col mt-20 lg:mt-28 gap-10 w-full min-h-80">
+					<div className="w-full">
+						<Title className="text-center">Departments</Title>
+					</div>
+					<div className="bg-transparent flex flex-wrap items-center justify-center gap-2 md:gap-3 w-full">
+						{DepartmentsList.map((val, i) => (
+							<DepartementCard key={i} {...val} />
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Departments;
---
+++ src/components/Home/sections/Reviews.tsx
@@ -0,0 +1,43 @@
+import TestimonialsCard from "@/components/cards/TestimonialsCard";
+import Title from "@/components/ui/Title";
+import { Testimonials } from "@/constants/Testimonials";
+import Image from "next/image";
+
+const ReviewsSection = () => {
+	return (
+		<div className="mt-20 lg:mt-36 px-6 md:px-28 w-full flex flex-col gap-10 items-center justify-center">
+			<div className="w-full flex items-center justify-center flex-col gap-1">
+				<Title>Some Reviews</Title>
+				<h3 className="text-lg lg:text-2xl font-semibold uppercase text-sky-500">
+					of our clients
+				</h3>
+			</div>
+			<div className="w-full flex items-center p-0 lg:p-14 justify-center gap-7 flex-col lg:flex-row">
+				<div className="w-full lg:w-1/2 flex flex-col items-start justify-center gap-6 border-r border-sky-500">
+					{Testimonials.map((val, i) => (
+						<TestimonialsCard key={i} {...val} />
+					))}
+				</div>
+				<div className="flex flex-col gap-3 w-full lg:w-1/2">
+					<Image src={"/icons/quote.svg"} alt="Quote" height={50} width={50} />
+					<div className="flex items-start gap-2 flex-col ml-14">
+						<p className="font-normal text-base text-slate-400">
+							The pediatrician was great with him and made him feel at ease, and
+							the entire staff was kind and attentive. I recently had to bring
+							my child to ProHealth for a minor injury, and I was so impressed
+							with the care he received.
+						</p>
+						<Image
+							src={"/icons/rating.svg"}
+							alt="Quote"
+							height={16}
+							width={90}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ReviewsSection;
