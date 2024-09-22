a3fb022519482fc6bdedf39a2dd2f3e2f81775f5
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-22 08:48:44 +0100

    added bold user name style

--- src/components/dashboard/TopNavDoctor.tsx
+++ src/components/dashboard/TopNavDoctor.tsx
@@ -7,7 +7,7 @@ const TopNavDoctor = () => {
 	return (
 		<div className="w-full flex items-center justify-between px-6 py-3 border-b border-blue/15 sticky top-0 z-[9] bg-background">
 			<span>
-				Welcome <span>John Doe</span>
+				Welcome <span className="font-semibold">Doctor John</span>
 			</span>
 			<div className="flex items-center gap-7">
 				<Bell />
