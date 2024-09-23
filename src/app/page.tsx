9c8cbcf29a3dbd7f368fde79b5d816e47c358356
GitOmonwalé <inesagbozo@gmail>
2024-09-23 12:38:31 +0100

    removing of footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import NavBar from "@/components/Navigation/NavBar";
 import CTA from "@/components/Home/sections/CTA";
 import FAQ from "@/components/Home/sections/FAQ";
 import Brands from "@/components/Home/sections/Brands";
-import Footer from "@/components/Navigation/Footer";
 
 export default function Home() {
 	return (
@@ -21,7 +20,6 @@ export default function Home() {
 			<CTA />
 			<FAQ />
 			<Brands />
-			<Footer />
 		</main>
 	);
 }
