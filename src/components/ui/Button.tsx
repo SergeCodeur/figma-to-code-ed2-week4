5d063d30612873019148cccaba3708dff74b49f6
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-23 07:08:10 +0100

    style update for responsiveness of authentication pages

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -41,7 +41,7 @@ const Button: React.FC<ButtonProps> = ({
 	return (
 		<button
 			{...props}
-			className={`inline-flex justify-center items-center gap-2.5 rounded-lg px-4 py-2.5 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-dark-blue focus:ring-offset-2 ${variantClasses} ${className}`}
+			className={`inline-flex justify-center items-center gap-2.5 rounded-lg px-4 py-2.5 text-xs md:text-base font-medium focus:outline-none focus:ring-2 focus:ring-dark-blue focus:ring-offset-2 ${variantClasses} ${className}`}
 		>
 			{children}
 		</button>
