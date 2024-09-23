e107ac0afb3be3ad2cd85e140d752ec4bf9d1e6e
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-23 15:12:23 +0100

    update style of responsive

--- src/components/dashboard/TopNavDoctor.tsx
+++ src/components/dashboard/TopNavDoctor.tsx
@@ -1,28 +1,29 @@
-import { Bell, Hamburger } from "@/assets/icons";
+import { Bell, Cancel, Hamburger } from "@/assets/icons";
 import { useSidebar } from "@/contexts/SidebarContext";
 import Image from "next/image";
 
 const TopNavDoctor = () => {
-	const { toggleSidebar } = useSidebar();
+	const { isOpen, toggleSidebar } = useSidebar();
 	return (
 		<div className="w-full flex items-center justify-between px-6 py-3 border-b border-blue/15 sticky top-0 z-[9] bg-background">
-			<span>
+			<span className="text-sm">
 				Welcome <span className="font-semibold">Doctor John</span>
 			</span>
-			<div className="flex items-center gap-7">
+			<div className="flex items-center gap-3 md:gap-7">
 				<Bell />
 				<Image
-					className="rounded-full"
-					src="/images/user.png"
-					alt="user image"
+					className="rounded-full object-cover object-top"
+					src="/images/doctor-user-image.png"
+					alt="doctor image"
 					width={50}
 					height={51}
+					loading="lazy"
 				/>
 				<button
-					className="w-9 h-9 lg:hidden bg-blue/20 rounded-ls flex justify-center items-center"
+					className="w-11 h-10 lg:hidden bg-blue/20 rounded-ls flex justify-center items-center"
 					onClick={toggleSidebar}
 				>
-					<Hamburger />
+					{isOpen ? <Cancel /> : <Hamburger />}
 				</button>
 			</div>
 		</div>
