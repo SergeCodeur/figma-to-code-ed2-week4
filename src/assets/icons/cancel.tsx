36758eebe614d190d1026e16ae93d0d8b8c28679
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-24 16:53:11 +0100

    chore(icons): Update Cancel icon component

--- src/assets/icons/cancel.tsx
+++ src/assets/icons/cancel.tsx
@@ -7,9 +7,9 @@ const Cancel = (props: IconProps) => {
 			<path
 				d="M15.8337 4.16656L4.16705 15.8332M4.16705 4.16656L15.8337 15.8332"
 				stroke="#274760"
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</IconBase>
 	);
