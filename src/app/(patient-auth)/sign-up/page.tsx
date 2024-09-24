19e776fda1fe975e5478ac6a1584fed7e02aa6c8
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-24 16:52:15 +0100

    refactor(auth): Replace custom login form with PatientSignUnForm component

--- src/app/(patient-auth)/sign-up/page.tsx
+++ src/app/(patient-auth)/sign-up/page.tsx
@@ -1,25 +1,11 @@
-"use client";
-import Arrow from "@/assets/icons/arrow";
-import Button from "@/components/ui/Button";
-import Input from "@/components/ui/Input";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { registerForm } from "../utils/data";
-// import useRegister from "./userRegister";
+import SignUpForm from "@/components/auth/PatientSignUpForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Sign Up",
+};
 
 const SignUp = () => {
-	const [animate, setAnimate] = useState(false);
-	// const { formData, errors, handleChange, handleSubmit } = useRegister();
-
-	const router = useRouter();
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setAnimate(true);
-		}, 100);
-		return () => clearTimeout(timer);
-	}, []);
 	return (
 		<div className="flex flex-col justify-center items-center space-y-10">
 			<div className="flex flex-col space-y-3 ">
@@ -31,62 +17,7 @@ const SignUp = () => {
 					appointments.
 				</p>
 			</div>
-			<form
-				className={`flex flex-col space-y-4 w-full ${
-					animate ? "move-in-left" : "opacity-0"
-				}`}
-				// onSubmit={handleSubmit}
-			>
-				{registerForm.map(({ name, label, placeholder }) => (
-					<div className="flex flex-col space-y-2" key={name}>
-						<label className="ml-2 text-[#414870] text-xs md:text-sm">
-							{label}
-						</label>
-						<Input
-							type={
-								name === "email"
-									? "email"
-									: name === "password"
-									? "password"
-									: "text"
-							}
-							name={name}
-							// value={formData[name as keyof typeof formData]}
-							// onChange={handleChange}
-							placeholder={placeholder}
-							required
-						/>
-						{/* {errors[name as keyof typeof formData] && (
-							<span className="text-sm text-red-500">
-								{errors[name as keyof typeof formData]}
-							</span>
-						)} */}
-					</div>
-				))}
-
-				<div className="flex flex-col space-y-2">
-					<Button
-						variant="linear_color"
-						className="w-[200px] !rounded-3xl"
-						type="submit"
-						onClick={() => {
-							router.push("/sign-in");
-						}}
-					>
-						Register
-						<Arrow />
-					</Button>
-					<span className="text-xs text-foreground">
-						Already have an account ?
-						<Link
-							href="/sign-in"
-							className="text-[#414870] ml-2 font-extrabold"
-						>
-							Log In
-						</Link>
-					</span>
-				</div>
-			</form>
+			<SignUpForm />
 		</div>
 	);
 };
