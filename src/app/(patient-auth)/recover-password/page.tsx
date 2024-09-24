fc0243462a410ec6cd201a4fb91f2b124f78de7d
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-24 07:03:42 +0100

    update path import

--- src/app/(patient-auth)/recover-password/page.tsx
+++ src/app/(patient-auth)/recover-password/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Input from "../ui/input";
 
 const RecoverPassword = () => {
 	const [isVisible, setIsVisible] = useState(false);
@@ -16,12 +16,12 @@ const RecoverPassword = () => {
 	return (
 		<div className="flex flex-col justify-center items-center space-y-10 overflow-hidden">
 			<div className="flex flex-col space-y-3 ">
-				<span className="font-extrabold text-3xl text-foreground">
+				<h2 className="font-extrabold text-3xl text-foreground">
 					Forgot Password
-				</span>
-				<span className="text-base text-foreground">
+				</h2>
+				<p className="text-sm lg:text-base text-foreground">
 					Enter your email to reset your password.
-				</span>
+				</p>
 			</div>
 			<div
 				className={`flex flex-col space-y-4 w-full p-2 ${
@@ -29,21 +29,22 @@ const RecoverPassword = () => {
 				}`}
 			>
 				<div className="flex flex-col space-y-2">
-					<label className="ml-2 text-[#414870] text-sm">Email</label>
+					<label className="ml-2 text-[#414870] text-xs md:text-sm">
+						Email
+					</label>
 
-					<Input
-						className="w-full px-3 py-2 border border-blue/50 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						placeholder="john@gmail.com"
-						type="email"
-					/>
+					<Input placeholder="john@gmail.com" type="email" />
 				</div>
 
 				<div className="flex space-x-2 items-center">
 					<Button variant="linear_color" className="w-full">
-						<span className="text-white text-base">Submit</span>
+						Submit
 					</Button>
-					<Link href="/sign-in" className="text-white text-base w-full">
-						<Button variant="default" className="w-full h-11">
+					<Link
+						href="/sign-in"
+						className="text-white text-sm md:text-base w-full"
+					>
+						<Button variant="default" className="w-full h-10 md:h-11">
 							Cancel
 						</Button>
 					</Link>
