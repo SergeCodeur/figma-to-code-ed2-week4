293372f4d21a66948f585a5f3e6f9af5a4650a26
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-09-24 16:43:23 +0100

    refactor(auth): Replace custom login form with RecoverPassword component

--- src/app/(patient-auth)/recover-password/page.tsx
+++ src/app/(patient-auth)/recover-password/page.tsx
@@ -1,18 +1,11 @@
-"use client";
-import Button from "@/components/ui/Button";
-import Input from "@/components/ui/Input";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import RecoverPasswordForm from "@/components/auth/RecoveryPasswordForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Recover Password",
+};
 
 const RecoverPassword = () => {
-	const [isVisible, setIsVisible] = useState(false);
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setIsVisible(true);
-		}, 100);
-		return () => clearTimeout(timer);
-	}, []);
 	return (
 		<div className="flex flex-col justify-center items-center space-y-10 overflow-hidden">
 			<div className="flex flex-col space-y-3 ">
@@ -23,33 +16,7 @@ const RecoverPassword = () => {
 					Enter your email to reset your password.
 				</p>
 			</div>
-			<div
-				className={`flex flex-col space-y-4 w-full p-2 ${
-					isVisible ? "fall-in" : "fall-in-init"
-				}`}
-			>
-				<div className="flex flex-col space-y-2">
-					<label className="ml-2 text-[#414870] text-xs md:text-sm">
-						Email
-					</label>
-
-					<Input placeholder="john@gmail.com" type="email" />
-				</div>
-
-				<div className="flex space-x-2 items-center">
-					<Button variant="linear_color" className="w-full">
-						Submit
-					</Button>
-					<Link
-						href="/sign-in"
-						className="text-white text-sm md:text-base w-full"
-					>
-						<Button variant="default" className="w-full h-10 md:h-11">
-							Cancel
-						</Button>
-					</Link>
-				</div>
-			</div>
+			<RecoverPasswordForm />
 		</div>
 	);
 };
