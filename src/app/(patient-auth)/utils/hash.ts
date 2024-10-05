3a6be2eaee83efc95cc4e1d53070860e1bdcd906
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-10-05 14:22:51 +0100

    restore hash passeword utilities

--- src/app/(patient-auth)/utils/hash.ts
+++ src/app/(patient-auth)/utils/hash.ts
@@ -1,12 +1,12 @@
-// const hashPassword = (password: string): string => {
-// 	return password.split("").reverse().join("") + "h@sh#4";
-// };
+const hashPassword = (password: string): string => {
+	return password.split("").reverse().join("") + "h@sh#4";
+};
 
-// const verifyPassword = (
-// 	inputPassword: string,
-// 	hashedPassword: string,
-// ): boolean => {
-// 	return hashPassword(inputPassword) === hashedPassword;
-// };
+const verifyPassword = (
+	inputPassword: string,
+	hashedPassword: string,
+): boolean => {
+	return hashPassword(inputPassword) === hashedPassword;
+};
 
-// export { hashPassword, verifyPassword };
+export { hashPassword, verifyPassword };
