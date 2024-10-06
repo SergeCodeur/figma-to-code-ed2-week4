c909b433ce210102fd631b215868fb43531907e1
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-10-06 17:43:32 +0100

    add others props for appointment card

--- src/types/AppointmentCardProps.ts
+++ src/types/AppointmentCardProps.ts
@@ -1,6 +1,12 @@
 export interface AppointmentCardProps {
+	id: string;
 	image: string;
 	name: string;
 	date: string;
 	raison: string;
+	address: string;
+	interest: string;
+	problem: string;
+	time: string;
+	onDecline?: () => void;
 }
